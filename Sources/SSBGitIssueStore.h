bf595455c741f3a8b363a366e86e5384d5c2957f
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-03-17 19:10:38 -0400

    Fix: Remove missing SSBMessage.h imports

--- Sources/SSBGitIssueStore.h
+++ Sources/SSBGitIssueStore.h
@@ -1,5 +1,4 @@
 #import <Foundation/Foundation.h>
-#import "SSBMessage.h"
 #import "SSBFeedStore.h"
 
 NS_ASSUME_NONNULL_BEGIN
--- Sources/SSBGitRepo.h
+++ Sources/SSBGitRepo.h
@@ -1,5 +1,4 @@
 #import <Foundation/Foundation.h>
-#import "SSBMessage.h"
 #import "SSBFeedStore.h"
 #import "SSBGitObjectStore.h"
 
